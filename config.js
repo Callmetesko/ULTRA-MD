09c8c4ab7e30fe2396a875a30b649e0de0b0ff9e
Callmetesko <159592234+Callmetesko@users.noreply.github.com>
2024-11-08 18:26:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '2348148798299';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -19,8 +19,8 @@ for (let i = 0; i < ownerlist.length; i++) {
     global.owner.push([ownerlist[i], true]);
 }
 //
-global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.botNumber = '923051391007' //replace your bot number here
+global.botname = process.env.BOTNAME || 'Tesko';
+global.botNumber = '2349128930328' //replace your bot number here
 
 global.mods = []
 global.prems = []
